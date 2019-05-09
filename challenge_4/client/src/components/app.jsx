59a57d1e5eba692f3ebf18495474707d650e1133
Chris Huang <chrishuang128@gmail.com>
2019-05-09 14:48:36 -0700

    generate table onto to the DOM

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,21 +1,36 @@
-import Square from './square.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-      player1:'',
-      player2:'',
-    };
+			player1: '',
+			player2: ''
+		};
+		this.tableGenerate = this.tableGenerate.bind(this);
+	}
+	tableGenerate() {
+		var row = [];
+		for (let y = 0; y < 6; y++) {
+			var space = [];
+			for (let x = 0; x < 7; x++) {
+				space.push(
+					<td key={x} y={y} x={x}>
+						Hi
+					</td>
+				);
+			}
+			row.push(<tr key={y}>{space}</tr>);
+		}
+		return row;
 	}
 
-
-
 	render() {
-		retur(
-			<div>
-				
-			</div>
+		return (
+			<table>
+				<tbody>{this.tableGenerate()}</tbody>
+			</table>
 		);
 	}
 }
--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -1,3 +1,8 @@
-import App from './components/app.jsx'
+import App from './components/app.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
+// ReactDOM.render(<div>hi there</div>, document.getElementById('app'))
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
